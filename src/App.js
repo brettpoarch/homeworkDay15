eb83cddf667ad93874b778292fb5ae986a5bd79a
brett poarch <brett.poarch@gmail.com>
2017-03-24 14:14:44 -0700

    adding tasks to list and removing from list working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+//import './remove.js'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+export default React.createClass({
+  getInitialState: function(){
+    return{
+      task: '',
+      list: []
+    }
+  },
+
+  handleChange: function(e){
+    this.setState({
+      task: e.target.value
+    })
+  },
+
+  handleSubmit: function(e){
+    e.preventDefault()
+    this.setState({
+      list: [this.state.task, ...this.state.list],
+      task: ''
+    })
+  },
+
+  handleClick: function(e){
+    this.setState({
+      list: this.state.list.filter(function(value) { 
+        return value !== e.target.value 
+
+      })
+    })
+  },
+
+  render: function(){
+    return(
+      <div className='container'>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' className='inputBox'placeholder='What needs to be done?' value={this.state.task} onChange={this.handleChange}></input>
+        </form>
+        <ul>
+          {this.state.list.map(function(list){
+            return <li><input type='checkbox' /*onCheck={this.handleCheck}*/ />{list}<button value={list} onClick={this.handleClick}>X</button></li>
+          }.bind(this))}
+        </ul>
       </div>
-    );
+   )
   }
-}
-
-export default App;
+})
